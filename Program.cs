4eb93de9c99746e0e40b9d055c1f6c55f32c5e32
Paul Allen <lance@Pauls-MacBook-Pro.local>
2024-03-29 18:28:24 -0400

    Docker compose working, Creating tags working, db not working correctly inside of docker file

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<BlogDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("BlogDbConnectionString")));
 
